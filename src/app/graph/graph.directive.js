e00f7a15453a26acec3e9a579eaa09554c92bd97
Jon Cairo <jon@Jons-MacBook-Pro.local>
2015-04-13 14:03:05 -0600

    Add links to nodes.

--- src/app/graph/graph.directive.js
+++ src/app/graph/graph.directive.js
@@ -6,24 +6,26 @@ angular.module('graph')
         restrict: 'E',
         replace: false,
         link: function (scope, element, attrs) {
-            // scope.$watch(scope.getData(), function(newValue, oldValue, scope) {
-            //    console.log("Watching");
-            // }, objectEquality);
+            var click = function (node) {
+              console.log(node);
+              var win = window.open(node.author_url, '_blank');
+              win.focus();
+            };
 
             var data = scope.getData();
 
             var width = 960,
-                height = 600;
+                height = 800;
 
             var color = d3.scale.category20();
 
             var force = d3.layout.force()
-                .charge(-900)
+                .charge(-1200)
                 .linkDistance(150)
                 .size([width, height]);
 
             var svg = d3.select("body").append("svg")
-                .attr("width", width)
+                .attr("width", '100%')
                 .attr("height", height);
 
             force
@@ -37,12 +39,15 @@ angular.module('graph')
               .attr("class", "link")
               .style("stroke-width", function(d) { return Math.sqrt(d.value); });
 
+
+
             var node = svg.selectAll(".node")
               .data(mockData.data.nodes)
               .enter().append("circle")
               .attr("class", "node")
-              .attr("r", 10)
-              .style("fill", function(d) { return color(d.group); })
+              .attr("r", function (d) { return d.h_index; })
+              .style("fill", function(d) { return color(d.h_index); })
+              .on("click", click)
               .call(force.drag);
 
             var texts = svg.selectAll("text.label")
